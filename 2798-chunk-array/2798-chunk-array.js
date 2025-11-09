08225e36a8a04757a1f29a807ebef165e9b25248
Chetan Chaudhari <chetanchaudhari9371@gmail.com>
2025-11-09 22:51:32 +0530

    Time: 56 ms (23.27%), Space: 56 MB (65.79%) - LeetHub

---
+++ 2798-chunk-array/2798-chunk-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    const result = [];
+    for(let i = 0; i <arr.length; i += size){
+        const chunk = arr.slice(i , i + size);
+        result.push(chunk);
+    }
+          return result;
+};
