7fc1a99bd56b3dd7b5d73315b0e87abbac5fa9d9
Chetan Chaudhari <chetanchaudhari9371@gmail.com>
2025-11-14 23:31:40 +0530

    Time: 68 ms (81.88%), Space: 63.6 MB (92.04%) - LeetHub

---
+++ 2804-compact-object/2804-compact-object.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function(obj) {
+    if(Array.isArray(obj)){
+        return obj.filter(Boolean).map(compactObject);
+    }
+
+
+    if(obj !== null && typeof obj === "object"){
+
+        let result = {};
+        for(let key in obj){
+            if(Boolean(obj[key])){
+                result[key]= compactObject(obj[key]);
+            }
+        }   
+        return result;
+    }
+       return obj;
+};
