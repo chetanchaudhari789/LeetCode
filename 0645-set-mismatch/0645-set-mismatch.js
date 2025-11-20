8607d392252be3d0d0f98dbc8a061cffd4237100
Chetan Chaudhari <chetanchaudhari9371@gmail.com>
2025-11-20 23:50:55 +0530

    Time: 5 ms (81.17%), Space: 58.2 MB (85.55%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    let n = nums.length;
+    let duplicate  = -1;
+    let missing = -1;
+    
+    let freq = new Array(n+1).fill (0);
+
+    for(let num of nums){
+        freq[num]++;
+        if (freq[num]=== 2){
+            duplicate = num;
+        }
+    }
+
+
+    for(let i = 1; i <= n ; i++){
+        if(freq[i]=== 0){
+            missing = i;
+            break;
+            
+        }
+    }
+
+    return [duplicate , missing];
+};
