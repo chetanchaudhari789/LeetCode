d0f7d8acf0c677bbe306e99c0cb4457c1f8309b9
Chetan Chaudhari <chetanchaudhari9371@gmail.com>
2025-12-04 00:09:58 +0530

    Time: 5 ms (64.56%), Space: 58.5 MB (45.73%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -3,23 +3,25 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    let stack = [];
+   let stack = [];
 
-    for (let t of tokens) {
-        if (t === "+" || t === "-" || t === "*" || t === "/") {
+   for(let t of tokens){
+    if( t === "+"|| t === "-" || t === "*" || t === "/"){
+        let b = stack.pop();
+        let a = stack.pop();
 
-            let b = stack.pop();
-            let a = stack.pop();
 
-            if (t === "+") stack.push(a + b);
-            else if (t === "-") stack.push(a - b);
-            else if (t === "*") stack.push(a * b);
-            else if (t === "/") stack.push(Math.trunc(a / b)); // truncate toward zero
+        if(t === "+") stack.push(a + b);
+        else if (t === "*") stack.push(a * b);
+        else if (t === "/") stack.push(Math.trunc(a / b));
+        else if (t === "-") stack.push(a - b);
 
-        } else {
-            stack.push(Number(t));
-        }
     }
+         else{
+                stack.push(Number(t))
+             }
+    } 
+   
 
-    return stack.pop();
+   return stack.pop();
 };
